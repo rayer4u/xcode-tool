94fc45b2ec449eb6fb5af3ef20fc622eb15926fe
ray <ray@rays-Mac.local>
2015-04-20 01:51:52 -0700

    合入LocaleString自动繁简体转换的脚本

---
+++ zhs2t.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+from __future__ import print_function
+import re,codecs,opencc
+
+
+
+repl = re.compile('"(.*?)"\s*=\s*"(.*?)"')
+with codecs.open('zh-Hans.lproj/Localizable.strings', 'r', encoding='utf-8') as f:
+    with codecs.open('zh-Hant.lproj/Localizable.strings', 'w', encoding='utf-8') as fw:
+	for line in f.readlines():
+	    result = repl.match(line)
+	    if result:
+		key = result.group(1) 
+	 	fw.write('"'+key+'" = "'+opencc.convert(key, config='s2t.json')+'";')
+	    else:
+		fw.write(line)
+
+
